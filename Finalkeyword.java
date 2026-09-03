77648badca48d9e2900317e47181cce5cd11370a
Ajay Raj Chauhan <89095436+ajayraj12342021@users.noreply.github.com>
2026-09-04 00:23:01 +0530

    Add files via upload

---
+++ Finalkeyword.java
@@ -0,0 +1,7 @@
+public class Finalkeyword {
+    public static void main(String[] args) {
+        final int x = 5;
+        // x = 10; // This would cause a compile-time error
+        System.out.println(x);
+    }
+}
